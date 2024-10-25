785895c90eafa171bb4b17146794fe11f78f85e4
loaisaber07 <loai.sabir@outlook.com>
2024-10-25 03:09:41 +0300

    Add Extension Method

---
+++ Chapter12DelegateAndEvenHandler/ExtensionMethid/Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter12DelegateAndEvenHandler.ExtensionMethid; 
+
+    public static class Numbers
+    {
+    public static IEnumerable<int> GetEvenNumbers(this IEnumerable<int> numbers, Predicate<int> isEven)
+    {
+        foreach (var item in numbers.Where(s=>isEven(s)))
+        {
+            yield return item;
+        }
+ 
+    }
+    }
+
--- Chapter12DelegateAndEvenHandler/Program.cs
+++ Chapter12DelegateAndEvenHandler/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Chapter12DelegateAndEvenHandler.Entity;
+using Chapter12DelegateAndEvenHandler.ExtensionMethid;
 
 namespace Chapter12DelegateAndEvenHandler
 {
@@ -6,15 +7,24 @@ namespace Chapter12DelegateAndEvenHandler
     {
         static void Main(string[] args)
         {
-         Exam ex = new Exam();
-         ex.AddQuesitonAndAnswer("Q1", new List<string> { "A1", "A2", "A3" });
-         ex.AddQuesitonAndAnswer("Q2", new List<string> { "A1", "A2", "A3" });
-         ex.AddQuesitonAndAnswer("Q3", new List<string> { "A1", "A2", "A3" });
-       Student student = new Student();
-            student.Name = "loai saber";
-            student.Age = 27;
-            ex.ExamEvent += student.ExamSubscribe;
-         ex.StartExam();
+            #region deligate 
+            //     Exam ex = new Exam();
+            //  ex.AddQuesitonAndAnswer("Q1", new List<string> { "A1", "A2", "A3" });
+            //  ex.AddQuesitonAndAnswer("Q2", new List<string> { "A1", "A2", "A3" });
+            //  ex.AddQuesitonAndAnswer("Q3", new List<string> { "A1", "A2", "A3" });
+            //Student student = new Student();
+            //     student.Name = "loai saber";
+            //     student.Age = 27;
+            //     ex.ExamEvent += student.ExamSubscribe;
+            //  ex.StartExam();
+            #endregion
+            #region Extenstion Method
+            List<int> intArray = new List<int>{ 2, 4, 5, 7, 8, 5, 3, };
+            foreach (var even in intArray.GetEvenNumbers(x => x % 2 == 0))
+            {
+                Console.WriteLine(even);
+            }
+            #endregion
         }
     }
 }
